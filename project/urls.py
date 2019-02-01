09d450de783840a6086a4efb044bc79ac837771f
rogierjobse <rogier.jobse@achmea.nl>
2019-02-01 15:50:22 +0100

    fixed bug

--- project/urls.py
+++ project/urls.py
@@ -9,9 +9,9 @@ urlpatterns = [
     # url(r'^$', 'project.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
-    path('/', index),
-    path('/health', health),
-    path('/admin', admin.site.urls),
+    path('', index),
+    path('health', health),
+    path('admin', admin.site.urls),
 ]
 
 #if settings.DEBUG:
